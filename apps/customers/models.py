7ccc1c43a6ed9544d46ae3e777e72cf2696ca7be
Ragavan-Ram <ragavan.ram@avantnovus.com>
2025-06-14 13:13:33 +0530

    customers model updated

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -16,10 +16,10 @@ class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
-        if not self.order_id:
+        if not self.customer_id:
             last_customer = Customer.objects.order_by("-id").first()
             next_id = 1 if not last_customer else last_customer.id + 1
-            self.order_id = f"ORD{next_id:04d}"
+            self.order_id = f"CUS{next_id:04d}"
         super().save(*args, **kwargs)
 
     def __str__(self):
