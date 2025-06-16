3b6ac7be8628b7b8702d8ecff21096e6b607f5f0
Ragavan-Ram <ragavan.ram@avantnovus.com>
2025-06-16 10:12:30 +0530

    token expiry time updated

--- core/settings.py
+++ core/settings.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=28),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
