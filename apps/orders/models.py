9b2d7ee9b5fc8d278c5325c485d6e417a3de1e46
Ragavan-Ram <ragavan.ram@avantnovus.com>
2025-09-16 18:56:50 +0530

    Unpaid Order Filter Bug Fix - 2

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -19,7 +19,10 @@ class OrderQuerySet(models.QuerySet):
         and filter only those with unpaid balance.
         """
         return (
-            self.filter(payments__payment_type="credit")
+            self.filter(
+                Q(payments__payment_type="credit")
+                | Q(payments__payment_type="repayment")
+            )
             .distinct()
             .annotate(
                 total_credit=Coalesce(
